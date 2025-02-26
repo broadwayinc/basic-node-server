4ae4b465e38af6b2fa7e7f1ed409354ff820cb1c
dev <dev@broadwayinc.com>
2025-02-26 13:08:19 +0000

    add optional 404 file handling and update documentation

--- bns.js
+++ bns.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const port = process.argv[2] || 3000; // Set the port from the command line argument or default to 3000
-
+const notFoundFile = process.argv?.[3] || null;
 const mimeJson = path.join(__dirname, '/mime.json');
 
 const getContentType = (() => {
@@ -43,8 +43,40 @@ const server = http.createServer((req, res) => {
     fs.access(filePath, fs.constants.F_OK, (err) => {
         if (err) {
             // File not found
-            res.statusCode = 404;
-            res.end('File not found');
+            if (notFoundFile) {
+                filePath = path.join(process.cwd(), notFoundFile);
+                fs.access(filePath, fs.constants.F_OK, (err) => {
+                    if (err) {
+                        res.statusCode = 404;
+                        res.end('File not found');
+                    } else {
+                        // Read the file and send it as the response
+                        const fileExtension = path.extname(filePath).substring(1);
+                        getContentType.then(contentType => {
+                            console.log(`Serving: ${filePath}`);
+
+                            contentType = contentType[fileExtension] || 'application/octet-stream';
+                            res.setHeader('Content-Type', contentType);
+
+                            fs.readFile(filePath, (err, data) => {
+                                if (err) {
+                                    // Error reading the file
+                                    res.statusCode = 500;
+                                    res.end('Internal server error');
+                                } else {
+                                    // Send the file contents as the response
+                                    res.statusCode = 200;
+                                    res.end(data);
+                                }
+                            });
+                        });
+                    }
+                });
+            }
+            else {
+                res.statusCode = 404;
+                res.end('File not found');
+            }
         } else {
             // Read the file and send it as the response
             const fileExtension = path.extname(filePath).substring(1);
